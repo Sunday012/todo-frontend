50a99d1035fb650cc9ee963134780217aeed41ac
Sunday favour <90037865+Sunday012@users.noreply.github.com>
2024-06-23 16:54:50 +0100

    update

--- src/_components/listTodo.tsx
+++ src/_components/listTodo.tsx
@@ -3,111 +3,82 @@ import { TodoCard } from "./todocard";
 import { EmptyTodo } from "./emptytodo";
 import { AuthContext } from "@/context/authContext";
 import { CreateTodo } from "./create-todo";
-type TodoProps = {
-  todo_id: number;
-  todo_name?: string;
-  description: string;
-  title: string;
-  deadline: string;
-  start_date: string;
-  deadline_time: string;
-};
 
-export default function Todo(){
-  const {auth} = useContext(AuthContext)
-  const {token} = auth;
-  useEffect(() => {
-    if (!token) {
-        window.location.href = "/login";
-    }
-}, [token]);
+export default function Todo() {
+    const { auth } = useContext(AuthContext);
+    const { token } = auth;
 
-  return(
-    <div>
-      <ListTodo />
-    </div>
-  )
+    useEffect(() => {
+        if (!token) {
+            window.location.href = "/login";
+        }
+    }, [token]);
+
+    return (
+        <div>
+            <ListTodo />
+        </div>
+    );
 }
 
 function ListTodo() {
-  const [todos, setTodos] = useState<any>([]);
-//   const setTodosWithType: React.Dispatch<SetStateAction<any[] | never[]>> = setTodos;
-  const {rows} = todos;
-  const {getTodo, todo, auth} = useContext(AuthContext)
-  const {loading} = auth;
-  const getTodos = async () => {
-    try {
-        await getTodo();
-      // console.log(data)
-      if(todo){
-        setTodos(todo);
-      }
-      
-    //   console.log([data]);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  useEffect(() => {
-    getTodos();
-  }, []);
+    const [todos, setTodos] = useState<any>([]);
+    const { getTodo, todo, auth } = useContext(AuthContext);
+    const { loading } = auth;
 
-useEffect(() => {
-    if (todo) {
-        setTodos(todo);
-    }
-}, [todo]);
+    useEffect(() => {
+        getTodo();
+    }, []);
 
-if(loading){
-  return(
-    <div className="w-full flex items-center justify-center mt-10">
-    <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 sm:grid-cols-2 gap-2 w-[80%]">
-      <TodoCard.Skeleton />
-      <TodoCard.Skeleton />
-      <TodoCard.Skeleton />
-      <TodoCard.Skeleton />
-      <TodoCard.Skeleton />
-      <TodoCard.Skeleton />
-    </div>
-    </div>
-  )
-}
+    useEffect(() => {
+        if (todo) {
+            setTodos(todo);
+        }
+    }, [todo]);
 
-  if (rows && rows.length > 0) {
-    console.log(todos)
-    return (
-      <div className="w-full flex items-center justify-center mt-10">
-        {/* <EmptyTodo /> */}
-        <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 sm:grid-cols-2 gap-2 w-[80%]">
-          {rows
-            ? rows.map((todo: TodoProps) => (
-                <div key={todo.todo_id}>
-                  <Suspense fallback={<TodoCard.Skeleton />}>
-                    <TodoCard
-                      description={todo.description}
-                      id={todo.todo_id}
-                      title={todo.title}
-                      deadline={todo.deadline}
-                      start_date={todo.start_date}
-                      deadline_time={todo.deadline_time}
-                    //   setTodos={setTodos}
-                      todos={todos}
-                    />
-                  </Suspense>
+    if (loading) {
+        return (
+            <div className="w-full flex items-center justify-center mt-10">
+                <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 sm:grid-cols-2 gap-2 w-[80%]">
+                    <TodoCard.Skeleton />
+                    <TodoCard.Skeleton />
+                    <TodoCard.Skeleton />
+                    <TodoCard.Skeleton />
+                    <TodoCard.Skeleton />
+                    <TodoCard.Skeleton />
                 </div>
-              ))
-            : null}
-            <CreateTodo />
-        </div>
-      </div>
-    );
-  }
+            </div>
+        );
+    }
 
-  if(rows && rows.length == 0){
-    return (
-        <EmptyTodo />
-    );
-  }
+    const { rows } = todos;
+
+    if (rows && rows.length > 0) {
+        return (
+            <div className="w-full flex items-center justify-center mt-10">
+                <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 sm:grid-cols-2 gap-2 w-[80%]">
+                    {rows.map((todo: any) => (
+                        <div key={todo.todo_id}>
+                            <Suspense fallback={<TodoCard.Skeleton />}>
+                                <TodoCard
+                                    description={todo.description}
+                                    id={todo.todo_id}
+                                    title={todo.title}
+                                    deadline={todo.deadline}
+                                    start_date={todo.start_date}
+                                    deadline_time={todo.deadline_time}
+                                    todos={todos}
+                                />
+                            </Suspense>
+                        </div>
+                    ))}
+                    <CreateTodo />
+                </div>
+            </div>
+        );
+    }
+
+    if (rows && rows.length === 0) {
+        return <EmptyTodo />;
+    }
 }
-
-
