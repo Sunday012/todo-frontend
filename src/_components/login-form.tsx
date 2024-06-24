28ccd33d96d254b65b84b7532fce9341fcd2f241
Sunday favour <90037865+Sunday012@users.noreply.github.com>
2024-06-24 16:11:26 +0100

    new changes

--- src/_components/login-form.tsx
+++ src/_components/login-form.tsx
@@ -4,7 +4,7 @@ import { Label } from "@/components/ui/label";
 import { Link } from "react-router-dom";
 import { useContext, useState } from "react";
 import { AuthContext } from "@/context/authContext";
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 // type TokenDataType = {
 //     id: string;
 //     username: string;
@@ -13,46 +13,72 @@ import {useNavigate} from "react-router-dom"
 //     token: TokenDataType
 // }
 export default function LoginForm() {
-  const navigate = useNavigate()
-    const [formData, setFormData] = useState({email:"", password:""})
-    const {email, password} = formData
-    const [isloading, setIsLoading] = useState(false)
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const { email, password } = formData;
+  const [isloading, setIsLoading] = useState(false);
 
-    const onChange = (e : any) => setFormData({...formData, [e.target.name]:e.target.value})
-    const {login} = useContext(AuthContext)
-    const LoginUser = async (e : any) => {
-      setIsLoading(true)
-        e.preventDefault();
-        await login(formData)
-        navigate("/mytodo")
-    }
+  const onChange = (e: any) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  const { login } = useContext(AuthContext);
+  const LoginUser = async (e: any) => {
+    e.preventDefault();
+    setIsLoading(true);
+    await login(formData);
+    navigate("/mytodo");
+  };
   return (
     <div className="w-full flex items-center justify-center">
-    <form action="" onSubmit={LoginUser} className="flex flex-col w-[50%] gap-4 items-center justify-center mt-10">
+      <form
+        action=""
+        onSubmit={LoginUser}
+        className="flex flex-col w-[50%] gap-4 items-center justify-center mt-10"
+      >
         <div className="w-full space-y-2">
-        <Label htmlFor="email" className="text-white font-semibold">Email</Label>
-        <Input type="email" placeholder="Email" id="email" name="email" value={email} onChange={onChange}/>
+          <Label htmlFor="email" className="text-white font-semibold">
+            Email
+          </Label>
+          <Input
+            type="email"
+            placeholder="Email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={onChange}
+          />
         </div>
         <div className="w-full space-y-2">
-        <Label htmlFor="password" className="text-white font-semibold">Password</Label>
-        <Input type="password" placeholder="Password" id="password" name="password" value={password} onChange={onChange} />
+          <Label htmlFor="password" className="text-white font-semibold">
+            Password
+          </Label>
+          <Input
+            type="password"
+            placeholder="Password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={onChange}
+          />
         </div>
-        <Button type="submit" className="bg-gradient-to-r w-full from-[#4EA8DE] to-[#5E60CE] text-white mt-2 py-2">Login</Button>
+        <Button
+          type="submit"
+          className="bg-gradient-to-r w-full from-[#4EA8DE] to-[#5E60CE] text-white mt-2 py-2"
+        >
+          Login
+        </Button>
         <div className="flex gap-2">
-            <p className="text-white">Don't have an account already?</p>
-            <Link to="/register" className="text-white">
-             Register
-            </Link>
+          <p className="text-white">Don't have an account already?</p>
+          <Link to="/register" className="text-white">
+            Register
+          </Link>
         </div>
-    </form>
-    {isloading ? (
-      <div className="flex h-screen items-center justify-center">
-      <div
-        className="h-10 w-10 animate-spin rounded-full border-4 border-blue-500 border-opacity-50"
-      ></div>
-      <div className="w-full h-screen inset-0 bg-inherit" />
+      </form>
+      {isloading ? (
+        <div className=" mt-24 absolute flex h-screen w-full items-center justify-center">
+          <div className=" mt-16 absolute w-full h-full bg-black/30 inset-0" />
+          <div className="h-10 w-10 animate-pulse rounded-full border-4 border-blue-500 border-opacity-50"></div>
+        </div>
+      ) : null}
     </div>
-    ): null}
-    </div>
-  )
+  );
 }
--- src/_components/register-form.tsx
+++ src/_components/register-form.tsx
@@ -6,53 +6,91 @@ import { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 export default function RegisterForm() {
-    const navigate = useNavigate()
-    const [isloading, setIsLoading] = useState(false)
-    const [formData, setFormData] = useState({username: "",email: "", password:""});
-    const {username, email, password} = formData;
-    const {register} = useContext(AuthContext)
-    const onChange = (e : any) => {
-        setFormData({...formData, [e.target.name]: e.target.value})
-    } 
+  const navigate = useNavigate();
+  const [isloading, setIsLoading] = useState(false);
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const { username, email, password } = formData;
+  const { register } = useContext(AuthContext);
+  const onChange = (e: any) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
-    const RegisterUser = async (e : any) => {
-        e.preventDefault()
-        setIsLoading(true)
-        console.log(formData)
-        await register(formData)
-        navigate("/login")
-    }
+  const RegisterUser = async (e: any) => {
+    e.preventDefault();
+    setIsLoading(true);
+    console.log(formData);
+    await register(formData);
+    navigate("/login");
+  };
   return (
     <div className="w-full flex items-center justify-center">
-    <form action="" onSubmit={RegisterUser} className="flex flex-col w-[50%] gap-4 items-center justify-center mt-10">
+      <form
+        action=""
+        onSubmit={RegisterUser}
+        className="flex flex-col w-[50%] gap-4 items-center justify-center mt-10"
+      >
         <div className="w-full space-y-2">
-        <Label htmlFor="text" className="text-white font-semibold">UserName</Label>
-        <Input type="text" placeholder="UserName" id="name" name="username" value={username} onChange={onChange} />
+          <Label htmlFor="text" className="text-white font-semibold">
+            UserName
+          </Label>
+          <Input
+            type="text"
+            placeholder="UserName"
+            id="name"
+            name="username"
+            value={username}
+            onChange={onChange}
+          />
         </div>
         <div className="w-full space-y-2">
-        <Label htmlFor="email" className="text-white font-semibold">Email</Label>
-        <Input type="email" placeholder="Email" id="email" name="email" value={email} onChange={onChange}/>
+          <Label htmlFor="email" className="text-white font-semibold">
+            Email
+          </Label>
+          <Input
+            type="email"
+            placeholder="Email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={onChange}
+          />
         </div>
         <div className="w-full space-y-2">
-        <Label htmlFor="password" className="text-white font-semibold">Password</Label>
-        <Input type="password" placeholder="Password" id="password" name="password" value={password} onChange={onChange} />
+          <Label htmlFor="password" className="text-white font-semibold">
+            Password
+          </Label>
+          <Input
+            type="password"
+            placeholder="Password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={onChange}
+          />
         </div>
-        <Button type="submit" className="bg-gradient-to-r w-full from-[#4EA8DE] to-[#5E60CE] text-white mt-2 py-2">Register</Button>
+        <Button
+          type="submit"
+          className="bg-gradient-to-r w-full from-[#4EA8DE] to-[#5E60CE] text-white mt-2 py-2"
+        >
+          Register
+        </Button>
         <div className="flex gap-2">
-            <p className="text-white">Have an account already?</p>
-            <Link to="/login" className="text-white">
-             Login
-            </Link>
+          <p className="text-white">Have an account already?</p>
+          <Link to="/login" className="text-white">
+            Login
+          </Link>
         </div>
-    </form>
-    {isloading ? (
-      <div className="flex h-screen items-center justify-center">
-      <div
-        className="h-10 w-10 animate-spin rounded-full border-4 border-blue-500 border-opacity-50"
-      ></div>
-      <div className="w-full h-screen inset-0 bg-inherit" />
+      </form>
+      {isloading ? (
+        <div className=" mt-24 absolute flex h-screen w-full items-center justify-center">
+          <div className=" mt-16 absolute w-full h-full bg-black/30 inset-0" />
+          <div className="h-10 w-10 animate-pulse rounded-full border-4 border-blue-500 border-opacity-50"></div>
+        </div>
+      ) : null}
     </div>
-    ): null}
-    </div>
-  )
+  );
 }
